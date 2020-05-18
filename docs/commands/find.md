b06ce8fb39ea52d10e14b2a95076fc20d63f7488
ishiahirake <ishiahirake@gmail.com>
2020-05-18 23:16:59 +0900

    修改侧边栏, 添加 find 草稿.

---
+++ docs/commands/find.md
@@ -0,0 +1,9 @@
+---
+title: Find
+---
+
+在文件夹中查找文件.
+
+## 参考
+
+- [findutils](https://www.gnu.org/software/findutils/manual/html_mono/find.html)
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,5 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+  docs: {
+    命令: ['commands/find']
   },
 };
